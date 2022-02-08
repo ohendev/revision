f3544746df4e7df773090a410fda127857b918ab
Laurent VIGNERON <laur.vigneron@gmail.com>
2022-02-08 11:06:49 -0400

    list show new and create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,28 @@ class ListsController < ApplicationController
   def index
     @lists = List.where(user: current_user)
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
