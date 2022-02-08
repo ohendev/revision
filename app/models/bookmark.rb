2003c92773fb23fc85d10829efe86ce1813bf673
Laurent VIGNERON <laur.vigneron@gmail.com>
2022-02-08 09:49:29 -0400

    associations and validations models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+  validates :comment, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, :preview, :rating, presence: true
+  validates :rating, inclusion: { in: 0..5 }
 end
