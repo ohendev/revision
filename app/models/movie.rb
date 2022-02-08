5d530daa8f690deeb27876a8771ddb9e26ff293d
Laurent VIGNERON <laur.vigneron@gmail.com>
2022-02-08 10:24:24 -0400

    db seed done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, :preview, :rating, presence: true
+  validates :title, uniqueness: true
   validates :rating, inclusion: { in: 0.0..10.0 }
 end
